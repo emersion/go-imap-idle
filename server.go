deb16bd1357ddb1cd0c5bc2a2ef148b57b365f0e
emersion <contact@emersion.fr>
2016-08-27 18:22:40 +0200

    Adds support for latest go-imap

--- server.go
+++ server.go
@@ -34,11 +34,11 @@ func (h *Handler) Handle(conn server.Conn) error {
 
 type extension struct{}
 
-func (ext *extension) Capabilities(state imap.ConnState) (caps []string) {
-	if state&imap.SelectedState != 0 {
-		caps = append(caps, Capability)
+func (ext *extension) Capabilities(c server.Conn) []string {
+	if c.Context().State&imap.AuthenticatedState != 0 {
+		return []string{Capability}
 	}
-	return
+	return nil
 }
 
 func (ext *extension) Command(name string) server.HandlerFactory {
